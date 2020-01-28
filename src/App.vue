325e1d7a13d584a9c9da30f60ba5c2584c981fd0
Nataliya <natalia.satsyuk@gmail.com>
2020-01-28 22:29:40 +0200

    'angry-commit'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img class="text-center" alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Catalogue /> 
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Catalogue from './components/Catalogue.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Catalogue
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif; 
   color: #2c3e50;
   margin-top: 60px;
 }
+
+img {
+  display: block;
+  margin: 0 auto
+}
+
+.container {
+    width: 1200px;
+    margin: 0 auto;  
+}
+
+.text-center {
+  text-align: center;
+}  
 </style>
---
+++ src/components/Catalogue.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="catalogue">
+
+		<div class="container">
+			<h1 class="text-center">Catalogue:</h1>
+			<ul id="list">
+				<li v-for="item in items"> {{ item.name }} </li>	
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Catalogue',
+		el: '#list',
+		data: {			
+			items: [
+				{ name: 'first' },
+				{ name: 'second' },
+				{ name: 'third' }
+			]
+		},
+		
+		// mounted() {
+		// 	axios.get("https://api.nytimes.com/svc/topstories/2/home.json?api-key=your_api_key")
+  //   		.then(response => {this.results = response.data.results})
+		// },
+
+		// methods: {
+		// 	getData: function(itemGetFromApi) {
+		// 		this.item = itemGetFromApi
+		// 	}
+		// }
+	}
+</script>
+
+<style scoped >
+	.container {
+		width: 1200px;
+		margin: 0 auto;
+	}
+
+	ul {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+</style>
+
